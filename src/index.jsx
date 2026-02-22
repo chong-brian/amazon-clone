c92431ecd54d97218f5931c20415749c7cd32e58
Brian Chong <12647941+chong-brian@users.noreply.github.com>
2026-02-21 19:48:23 -0500

    migrate: replace react-scripts with Vite
    - Uninstall react-scripts, install vite + @vitejs/plugin-react
    - Upgrade react + react-dom to v18
    - Add vite.config.js
    - Move index.html to project root, remove %PUBLIC_URL% tokens, add module script entry
    - Rename src/*.js to *.jsx
    - Update src/index.jsx to use ReactDOM.createRoot (React 18 API)
    - Update package.json scripts: start/build/preview
    - Add dist/ to .gitignore

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { StateProvider } from './StateProvider';
+import reducer, { initialState } from './reducer';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>
+);
